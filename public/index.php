d09541c5a4f0380df61d5d3060ef0c4c6482c422
Deiby <pequelezcano12@gmail.com>
2026-04-20 18:20:11 -0300

    Add output buffering fix headers

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+ob_start();
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
+ini_set('display_errors', 0);
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../routes/web.php';
